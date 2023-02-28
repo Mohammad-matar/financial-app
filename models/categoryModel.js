2a0ddb7d6ae8ac295a2493cc7805d83be0da069a
Matarov <moe.matar1998@gmail.com>
2023-02-28 23:24:05 +0200

    editing category model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categorySchema = new mongoose.Schema({
+    
     title: {
         type: String,
         required: [true, "Please enter the title"],
         minLength: 3,
         trim: true
     },
-    
-    
+    type: {
+        type: String,
+        default: "expense",
+        enum: ["income", "expense"],
+    },
+
+    isDefault: {
+        type: Boolean,
+        default: false,
+    },
+
     user_id: {
         type: Schema.Types.ObjectId,
         ref: "User",
@@ -20,7 +30,6 @@ const categorySchema = new mongoose.Schema({
         timestamps: true
     }
 );
-
 
 
 module.exports = mongoose.model("Category", categorySchema);
